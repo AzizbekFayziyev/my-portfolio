3d7813de318e136fa26efffafb49fd4d55e1bc1c
Azizbek Fayziyev <avancoder@gmail.com>
2024-03-27 01:13:41 +0500

    removed: if condition

--- src/components/UI/PageIndicator.tsx
+++ src/components/UI/PageIndicator.tsx
@@ -6,16 +6,12 @@ const PageIndicator = () => {
   const { scrollYProgress } = useScroll();
   const scrollY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
-  if (navigator.maxTouchPoints) {
-    return null;
-  } else {
     return (
       <motion.div
         style={{ transform: "translate(0,-50%)", height: scrollY }}
-        className={`w-2 bg-primary fixed z-[999] right-0 top-[50%]`}
+        className={`w-2 bg-primary fixed z-[999] right-0 top-[50%] hidden md:block`}
       ></motion.div>
     );
-  }
 };
 
 export default PageIndicator;
